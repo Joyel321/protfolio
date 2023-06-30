d50fba221ebbb3db51b276034140dd5ce82ffe40
Joyel321 <134682922+Joyel321@users.noreply.github.com>
2023-06-30 21:56:24 +0530

    add

---
+++ protfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipe-text",{
+    strings: ["Frontend Developer", "Software Tester", "Debugger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
